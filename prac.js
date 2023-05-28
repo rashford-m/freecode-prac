e59bde219c7567a90851b3f9b754178c58ce5a0d
Rashford <mikearowo@gmail.com>
2023-05-28 08:42:55 +0100

    updated

--- prac.js
+++ prac.js
@@ -7,4 +7,8 @@ const myDecimal = 5.7;
 var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 const myStr = ""; // Change this line
 const ourStr = "I come first. " + "I come second.";
-const str = "I came first." + "I came second" 
+const str = "I came first." + "I came second";
+
+let mine = "I came first";
+mine += "I came second";
+
